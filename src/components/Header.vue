73cbf2233cd904a367ba0b173adc10f98a4cd4a9
gong <2039493706@qq.com>
2023-04-10 23:12:57 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,149 @@
+<template>
+	<div class="header-wrapper">
+		<img class="logo" :src="logoUrl" @click="$router.push({ name: 'Home' })" />
+		<img
+			class="logo-xs"
+			:src="logoUrlXs"
+			@click="$router.push({ name: 'Home' })"
+		/>
+		<el-menu
+			:default-active="activeIndex"
+			class="top-menu-list"
+			mode="horizontal"
+			@select="menuItemClick"
+		>
+			<el-menu-item index="File">破盘</el-menu-item>
+
+			<li class="el-menu-item external-link">
+			</li>
+			<template v-if="isLogin">
+				<el-submenu
+					class="user-exit-submenu"
+					index="User"
+					v-if="screenWidth <= 768"
+				>
+					<template slot="title">
+						<i class="el-icon-user-solid"></i>
+						<span>{{ username }}</span>
+					</template>
+					<el-menu-item @click="exitButton()">退出</el-menu-item>
+				</el-submenu>
+				<template v-else>
+					<!-- 为了和其他菜单样式保持一致，请一定要添加类名 el-menu-item -->
+					<div class="el-menu-item exit" @click="exitButton()">退出</div>
+					<div class="el-menu-item username" v-show="isLogin">
+						<i class="el-icon-user-solid"></i> <span>{{ username }}</span>
+					</div>
+				</template>
+			</template>
+
+			<!-- 开发环境 -->
+			<el-menu-item class="login" index="Login" v-show="!isLogin"
+				>登录</el-menu-item
+			>
+
+			<!-- 开发环境 -->
+			<el-menu-item class="register" v-show="!isLogin" index="Register"
+				>注册</el-menu-item
+			>
+		</el-menu>
+	</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+	name: 'Header',
+	data() {
+		return {
+			logoUrl: require('_a/images/common/logo_header.png'),
+			logoUrlXs: require('_a/images/common/logo_header_xs.png')
+		}
+	},
+	computed: {
+		...mapGetters(['isLogin', 'username']),
+		// 当前激活菜单的 index
+		activeIndex() {
+			return this.$route.name || 'Home' //  获取当前路由名称
+		},
+		// 屏幕宽度
+		screenWidth() {
+			return this.$store.state.common.screenWidth
+		}
+	},
+	methods: {
+
+		/**
+		 * 退出登录
+		 * @description 清除 cookie 存放的 token  并跳转到登录页面
+		 */
+		exitButton() {
+			this.$message.success('退出登录成功！')
+			this.$common.removeCookies(this.$config.tokenKeyName)
+			this.$store.dispatch('getUserInfo').then(() => {
+				this.$router.push({ name: 'Home' })
+			})
+		},
+
+		menuItemClick(key) {
+			if (key === 'exit') {
+				this.exitButton()
+			} else if (key === 'Login') {
+				this.$router.push({ name: key })
+			} else if (key === 'Register') {
+				this.$router.push({ name: key })
+			} else if (key === 'File') {
+				this.$router.push({ name: key, query: { fileType: 0, filePath: '/' } })
+			} else {
+				this.$router.push({ name: key })
+			}
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~_a/styles/varibles.styl';
+
+.header-wrapper {
+  width: 100%;
+  padding: 0 20px;
+  box-shadow: $tabBoxShadow;
+  display: flex;
+
+  .logo {
+    margin: 14px 24px 0 24px;
+    display: inline-block;
+    height: 40px;
+    cursor: pointer;
+  }
+
+  .logo-xs {
+    display: none;
+  }
+
+  >>> .el-menu--horizontal {
+    .el-menu-item:not(.is-disabled):hover {
+      border-bottom-color: $Primary !important;
+      background: $tabBackColor;
+    }
+
+    .external-link {
+      padding: 0;
+      a {
+        display: block;
+        padding: 0 20px;
+      }
+    }
+  }
+
+  .top-menu-list {
+    flex: 1;
+
+    .login, .register, .username, .exit, .user-exit-submenu {
+      float: right;
+    }
+  }
+}
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,74 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+
+    {
+      path: '/',
+      name: 'File',
+      component: () => import(/* webpackChunkName: "file" */ '_v/File.vue'),
+      meta: {
+        requireAuth: true, //  当前路由是否需要登录才可进入
+        title: '网盘',
+        content: {
+          description: '图片 文档 视频 音乐 其他 回收站 我的分享'
+        }
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import(/* webpackChunkName: "login" */ '_v/Login.vue'),
+      meta: { title: '登录' }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: () =>
+        import(/* webpackChunkName: "register" */ '_v/Register.vue'),
+      meta: { title: '注册' }
+    },
+
+    {
+      path: '/onlyoffice',
+      name: 'Onlyoffice',
+      meta: {
+        title: '在线编辑预览',
+        content: {
+          description: 'onlyoffice 文档在线编辑预览，支持 Word Excel PowerPoint'
+        }
+      },
+      component: () =>
+        import(/* webpackChunkName: "onlyOffice" */ '_v/OnlyOffice.vue')
+    },
+    {
+      path: '/share/:shareBatchNum',
+      name: 'Share',
+      component: () => import(/* webpackChunkName: "share" */ '_v/Share.vue'),
+      meta: {
+        title: '分享',
+        content: {
+          description: '查看他人分享'
+        }
+      },
+      props: true
+    },
+    {
+      path: '*',
+      name: 'Error_404',
+      component: () =>
+        import(/* webpackChunkName: "error_404" */ '_v/ErrorPage/404.vue'),
+      meta: { title: '链接不存在' }
+    }
+  ]
+})
+
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
